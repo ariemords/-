2546623bc6f0ac53d20aee9ab2afa338a4ae007a
ariemords <ariemords@gmail.com>
2026-02-23 21:25:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function showLogin() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("login").style.display = "block";
+}
 
+function enterSite() {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}
