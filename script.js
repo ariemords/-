a64d57a9bf3f7807339ac04634415b4cfbfcfb0f
ariemords <ariemords@gmail.com>
2026-02-23 21:32:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function enterSite() {
   document.getElementById("login").style.display = "none";
   document.getElementById("main").style.display = "block";
 }
+function showBorrow() {
+  document.getElementById("main").style.display = "none";
+  document.getElementById("borrowScreen").style.display = "block";
+}
+
+function showDonate() {
+  document.getElementById("main").style.display = "none";
+  document.getElementById("donateScreen").style.display = "block";
+}
+
+function backToMain() {
+  document.getElementById("borrowScreen").style.display = "none";
+  document.getElementById("donateScreen").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}
